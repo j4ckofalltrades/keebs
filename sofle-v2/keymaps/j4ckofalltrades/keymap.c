21c924330473b80515fb87fd3e2ce05c24f69a5a
Jordan Duabe <jordan.duabe@gmail.com>
2022-01-23 22:17:43 +0800

    Update Sofle keymap

--- sofle-v2/keymaps/j4ckofalltrades/keymap.c
+++ sofle-v2/keymaps/j4ckofalltrades/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
+ * |  `   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  |   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------| TO(2) |    | TO(1) |------+------+------+------+------+------|
@@ -34,8 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [0] = LAYOUT(
-  KC_GRV,               KC_1,    KC_2,    KC_3,     KC_4,  KC_5,                   KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_GRV,
-  KC_TAB,               KC_Q,    KC_W,    KC_E,     KC_R,  KC_T,                   KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPC,
+  KC_GRV,               KC_1,    KC_2,    KC_3,     KC_4,  KC_5,                   KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
+  KC_TAB,               KC_Q,    KC_W,    KC_E,     KC_R,  KC_T,                   KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS,
   MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,     KC_F,  KC_G,                   KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,              KC_Z,    KC_X,    KC_C,     KC_V,  KC_B, TO(2),     TO(1), KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT,
                         KC_LGUI, KC_LALT, KC_LCTRL, MO(2), KC_SPC,                 KC_ENT,  MO(1),   KC_RCTRL, KC_RALT, KC_RGUI
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'            '------''---------------------------'
  */
 [1] = LAYOUT(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  CG_TOGG, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
   _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,     _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
